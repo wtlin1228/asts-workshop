e1c52d8b1d3d3a4b87bfb05b92fa7798d7f8074d
林暐唐 <wtlin1228@mini.local>
2020-08-07 20:09:54 +0800

    ready to go

--- other/template-test.js
+++ other/template-test.js
@@ -5,7 +5,7 @@
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=ASTs&e=&em=
+http://ws.kcd.im/?ws=ASTs&e=&em=wtlin1228@gmail.com
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
